f6313fe4b99ac7deff85736e328860bda47ca3e5
Ben Wang <ben.wang@emc.com>
2017-02-27 11:36:22 -0800

    temporarily add pravega's dependencies

--- lib/logstash/outputs/pravega.rb
+++ lib/logstash/outputs/pravega.rb
@@ -6,6 +6,12 @@ require "java"
 require "client"
 require "common"
 require "contract"
+# TODO other pravega dependencies
+require "commons-lang-2.6"
+require "guava-16.0"
+require "libthrift-0.9.1"
+require "netty-all-4.0.36.Final"
+require "slf4j-api-1.7.14"
 
 class LogStash::Outputs::Pravega < LogStash::Outputs::Base
   declare_threadsafe!
